74f3eb6e5666aece7f560b2586d01816d8bef0b4
BillerJully <billerjully@gmail.com>
2022-09-22 12:26:14 +0300

    working

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
-# This is a sample Python script.
+import cv2
+import pytesseract
+import sys
+from imutils import contours
+pytesseract.pytesseract.tesseract_cmd = 'D:\\tesseract\\tesseract.exe'
+image = cv2.imread('./PlatesPic/3.jpg')
+height, weight, _ = image.shape
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU)[1]
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+contour = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+contour, _ = contours.sort_contours(contour[0])
+for i in contour:
+    area = cv2.contourArea(i)
+    x, y, w, h = cv2.boundingRect(i)
+    if area > 500:
+        img = image[y:y+h, x:x+w]
+        result = pytesseract.image_to_string(img, lang="eng+rus")
+        if len(result) > 7:
+            print(result)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
+cv2.imshow("Test", thresh)
+
+
+
+cv2.waitKey(0);
